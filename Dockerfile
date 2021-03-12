492b1efe0a89749edd9adacf30afe16b7c7eaa2f
Fraser, Steven (CarnCorp) <Sfraser@carnival.com>
2021-03-12 13:20:28 -0500

    adding kvm-node-driver config

--- Dockerfile
+++ Dockerfile
@@ -7,3 +7,5 @@ RUN apt update \
 #COPY THE JAILER FILE THAT MOUNTS THE UNRAID SERVER TO PASS THE DISK AN ISO
 COPY jailer.sh /usr/bin/jailer.sh
 RUN chmod +x /usr/bin/jailer.sh
+COPY kvm-node-driver.yaml /home/kvm-node-driver.yaml
+
--- jailer.sh
+++ jailer.sh
@@ -9,6 +9,13 @@ if [[ -z "$1" ]]
     exit 1
 fi
 
+
+if [ "$NAME" == "driver-jail" ]; then
+    k3s kubectl apply -f /home/kvm-node-driver.yaml
+fi
+
+
+
 # Build the jail directory structure
 mkdir -p /opt/jail/$NAME/dev
 mkdir -p /opt/jail/$NAME/etc/ssl/certs
